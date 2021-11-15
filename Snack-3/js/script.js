c58ccacf51d31a5c32a671cec4f2fe488624056b
Alex <adrdra88@gmail.com>
2021-11-15 18:00:49 +0100

    snack 3 soluzione abbreviata

--- Snack-3/js/script.js
+++ Snack-3/js/script.js
@@ -1,8 +1,13 @@
-let potenza = 2;
+// let potenza = 2;
 
-while( potenza < 1000){
-    potenza = potenza * 2;
-    if(potenza < 1000){
-        console.log(potenza)
-    }
+// while( potenza < 1000){
+//     potenza = potenza * 2;
+//     if(potenza < 1000){
+//         console.log(potenza)
+//     }
+// }
+
+
+for(i = 2; i < 1000; i *=2 ){
+    console.log(i)
 }
