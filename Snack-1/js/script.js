9860cca28288898705584c48071a77933f92dc59
Alex <adrdra88@gmail.com>
2021-11-15 16:08:13 +0100

    snack 1

---
+++ Snack-1/js/script.js
@@ -0,0 +1,22 @@
+
+// let somma = 0;
+
+// for(i = 0; i < 5; i++){
+
+//     const insNumber = parseInt(prompt('inserisci numero'));
+
+//     somma = somma + insNumber;
+
+  
+// }
+// console.log(somma);
+
+let i = 0;
+let somma = 0;
+
+while(i < 5){
+    const insNumber = parseInt(prompt('inserisci numero'));
+    somma = somma + insNumber;
+    i++
+}
+console.log(somma);
